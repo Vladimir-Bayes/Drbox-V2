2361d6b4e7d7b72d413d48bac24a4cf21a8a08aa
dc <divya.chandana@gmail.com>
2020-03-10 11:39:44 -0400

    changes

--- Drbox.py
+++ Drbox.py
@@ -3,18 +3,20 @@ import os.path
 import sys
 import random
 import numpy as np
-# np.load('./det1.npy')
+np.load('/det1.npy')
+# https://github.com/davidsandberg/facenet/tree/master/src/align
 from glob import glob
 import tensorflow as tf
 from model import *
 from rbox_functions import *
 import scipy.misc
 import pickle
+# ftp://mi.eng.cam.ac.uk/pub/mttt2/models/vgg16.npy
 
 TXT_DIR = './data' 
 INPUT_DATA_PATH = TXT_DIR + '/train'
 TEST_DATA_PATH = TXT_DIR + '/test'
-PRETRAINED_NET_PATH = "./vgg16.npy"
+PRETRAINED_NET_PATH = "/vgg16.npy"
 SAVE_PATH = './result' 
 TRAIN_BATCH_SIZE = 8
 IM_HEIGHT = 300
